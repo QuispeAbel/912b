606c28a1a5cddc84491596542a04850e8da346aa
QuispeAbel <quisperobledoabel@gmail.com>
2024-09-14 11:38:11 -0300

    boludes

--- app/src/main/java/com/example/a912b/ui/recordvideo/RecordVideoFragment.kt
+++ app/src/main/java/com/example/a912b/ui/recordvideo/RecordVideoFragment.kt
@@ -1,26 +1,18 @@
 package com.example.a912b.ui.recordvideo
 
-import androidx.fragment.app.viewModels
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
 import com.example.a912b.BotonesVideoFragment
 import com.example.a912b.R
-import com.example.a912b.databinding.FragmentHomeBinding
-import com.example.a912b.databinding.FragmentRecordVideoBinding
-import com.example.a912b.ui.recordvideo.frontal.CamFrontalFragment
 
 class RecordVideoFragment : Fragment() {
 
 
     companion object {
-        fun newInstance() = RecordVideoFragment()
     }
-
-    private val viewModel: RecordVideoViewModel by viewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
